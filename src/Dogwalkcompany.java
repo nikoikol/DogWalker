4f454ba8df2e41dc825e57ae3c42eba2147b8da1
NikolayShevchenko <nikolays2@ncystudents.net>
2025-09-22 15:32:50 -0400

    i did stuff

---
+++ src/Dogwalkcompany.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Dogwalkcompany
+{
+    private int[] schedule;
+    public Dogwalkcompany()
+    {
+        schedule = new int[24];
+    }
+    public String toString()
+    {
+        String s = "";
+        for (int i = 0; i < schedule.length; i++)
+            s += i + " " + schedule[i] + "\n";
+        return s;
+    }
+    public int addDogs()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hour?");
+        int hour = s.nextInt();
+        System.out.println("Dogs?");
+        int dogs = s.nextInt();
+        schedule[hour] = dogs;
+        return hour;
+    }
+}
---
+++ src/Dogwalker.java
@@ -0,0 +1,11 @@
+public class Dogwalker {
+    private int maxDogs;
+
+    private Dogwalkcompany;
+
+    public Dogwalker (int max, Dogwalkcompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+}
